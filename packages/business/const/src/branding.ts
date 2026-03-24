6243b8cdd8f914f6b58ef393a96496749a52871f
gilsonblg <gilsonrdsc@gmail.com>
2026-03-24 01:10:34 -0300

    branding: customização Radnet

--- packages/business/const/src/branding.ts
+++ packages/business/const/src/branding.ts
@@ -1,14 +1,10 @@
 // the code below can only be modified with commercial license
 // if you want to use it in the commercial usage
 // please contact us for more information: hello@lobehub.com
-
-export const LOBE_CHAT_CLOUD = 'LobeHub Cloud';
-
-export const BRANDING_NAME = 'LobeHub';
+export const LOBE_CHAT_CLOUD = 'IA Cloud';
+export const BRANDING_NAME = 'IA';
 export const BRANDING_LOGO_URL = '';
-
-export const ORG_NAME = 'LobeHub';
-
+export const ORG_NAME = 'Radnet Telecom';
 export const BRANDING_URL = {
   help: undefined,
   privacy: undefined,
@@ -16,25 +12,20 @@ export const BRANDING_URL = {
   support: undefined,
   terms: undefined,
 };
-
 export const SOCIAL_URL = {
-  discord: 'https://discord.gg/AYFPHvv2jT',
-  github: 'https://github.com/lobehub',
-  medium: 'https://medium.com/@lobehub',
-  x: 'https://x.com/lobehub',
-  youtube: 'https://www.youtube.com/@lobehub',
+  discord: undefined,
+  github: undefined,
+  medium: undefined,
+  x: undefined,
+  youtube: undefined,
 };
-
 export const FILE_URL = {
   importFromNotionGuide: 'https://hub-apac-1.lobeobjects.space/assets/notion.mp4',
 };
-
 export const BRANDING_EMAIL = {
-  business: 'hello@lobehub.com',
-  support: 'support@lobehub.com',
+  business: 'contato@viaradnet.com.br',
+  support: 'gilson@viaradnet.com.br',
 };
-
-export const BRANDING_PROVIDER = 'lobehub';
-
+export const BRANDING_PROVIDER = 'radnet';
 export const COPYRIGHT = `© ${new Date().getFullYear()} ${ORG_NAME}`;
 export const COPYRIGHT_FULL = `${COPYRIGHT}. All rights reserved.`;
