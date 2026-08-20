208af0b5bd9485ceca7e0368d8f7a4078a74f0e4
yutengjing <ytj2713151713@gmail.com>
2026-08-21 00:00:38 +0800

    ♻️ refactor: keep agent header inside feature

--- src/features/AgentConversation/Header/HeaderActions/index.tsx
+++ src/features/AgentConversation/Header/HeaderActions/index.tsx
@@ -5,8 +5,7 @@ import { ActionIcon } from '@lobehub/ui/base-ui';
 import { MoreHorizontal } from 'lucide-react';
 import { memo } from 'react';
 
-import HeaderSlot from '@/routes/(main)/agent/(chat)/_layout/HeaderSlot';
-
+import HeaderSlot from '../HeaderSlot';
 import { useMenu } from './useMenu';
 
 const HeaderActions = memo(() => {
